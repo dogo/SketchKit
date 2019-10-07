40642d6a971fb0c56e8e99a229717e5a98cdb41d
Diogo Autilio <diautilio@gmail.com>
2019-10-07 15:50:13 -0300

    Remove unsupported platform Adjust available support for tvOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SketchKit",
-    platforms: [.iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
--- Sources/SketchKit/Extensions/Anchors+SafeArea.swift
+++ Sources/SketchKit/Extensions/Anchors+SafeArea.swift
@@ -11,56 +11,56 @@ import UIKit
 public extension UIView {
 
     var safeTopAnchor: NSLayoutYAxisAnchor {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.topAnchor
         }
         return topAnchor
     }
 
     var safeBottomAnchor: NSLayoutYAxisAnchor {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.bottomAnchor
         }
         return bottomAnchor
     }
 
     var safeLeadingAnchor: NSLayoutXAxisAnchor {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.leadingAnchor
         }
         return leadingAnchor
     }
 
     var safeTrailingAnchor: NSLayoutXAxisAnchor {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.trailingAnchor
         }
         return trailingAnchor
     }
 
     var safeCenterXAnchor: NSLayoutXAxisAnchor {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.centerXAnchor
         }
         return centerXAnchor
     }
 
     var safeCenterYAnchor: NSLayoutYAxisAnchor {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.centerYAnchor
         }
         return centerYAnchor
     }
 
     var safeWidthAnchor: NSLayoutDimension {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.widthAnchor
         }
         return widthAnchor
     }
 
     var safeHeightAnchor: NSLayoutDimension {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
             return safeAreaLayoutGuide.heightAnchor
         }
         return heightAnchor
