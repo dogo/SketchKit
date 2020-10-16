5f3d49654d714976980f3a8f0d163d0e34c693d8
Diogo Autilio <diautilio@gmail.com>
2020-10-15 23:43:03 -0300

    Run unit testes with ios 9 and iPhone 6S

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,16 @@
 
 set -eo pipefail
 
+if [ "$1" = "latest" ]
+then
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
             clean test | xcpretty
+elif [ "$2" = "ios9" ]
+then
+xcodebuild -project SketchKit.xcodeproj \
+			-scheme SketchKit-Package \
+            -destination platform=iOS\ Simulator,OS=9.0,name=iPhone\ 6s \
+            clean test | xcpretty    
+fi               
