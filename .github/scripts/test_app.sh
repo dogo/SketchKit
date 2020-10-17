9d2f04b4bcaede59e4acb099437b53521a0395c3
Diogo Autilio <diautilio@gmail.com>
2020-10-17 13:15:29 -0300

    Adjust iOS version from test step

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
-            -destination platform=iOS\ Simulator,OS=10.3,name=iPhone\ 6s \
+            -destination platform=iOS\ Simulator,OS=10.3.1,name=iPhone\ 6s \
             clean test | xcpretty          
