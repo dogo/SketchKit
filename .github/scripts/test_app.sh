afba61242ae146ac0bc814914653efcad20dec59
Diogo Autilio <diautilio@gmail.com>
2020-10-17 12:18:31 -0300

    Run unit tests with two destinations

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,16 +2,8 @@
 
 set -eo pipefail
 
-if [ "$1" = "latest" ]
-then
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
-            clean test | xcpretty
-elif [ "$2" = "ios9" ]
-then
-xcodebuild -project SketchKit.xcodeproj \
-			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=9.0,name=iPhone\ 6s \
-            clean test | xcpretty    
-fi               
+            clean test | xcpretty          
