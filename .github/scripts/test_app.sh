7ec28603160c3fee1c7abc329607b277598b8518
Diogo Autilio <diautilio@gmail.com>
2021-02-19 09:14:48 -0300

    Upgrade Xcode version

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,6 +4,5 @@ set -eo pipefail
 
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
-            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
-            #-destination platform=iOS\ Simulator,OS=10.3.1,name=iPhone\ 6s \
+            -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
             clean test | xcpretty          
--- Gemfile
+++ Gemfile
@@ -4,6 +4,5 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "xcode-install"
 gem "xcpretty"
 gem "jazzy"
