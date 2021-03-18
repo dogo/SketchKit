5252bfaf6676e6b530d85c330ac460e70696d93e
Diogo Autilio <diautilio@gmail.com>
2021-03-18 19:40:34 -0300

    Remove xcpretty from test_app script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
-            clean test | xcpretty          
+            clean test
