6b9921a1ebd278678cdb33baebae12eef720d85f
Diogo Autilio <diautilio@gmail.com>
2021-10-20 19:30:09 -0300

    Update SketchKitDSL.swift

--- Sources/SketchKit/SketchKitDSL.swift
+++ Sources/SketchKit/SketchKitDSL.swift
@@ -9,17 +9,31 @@
 #if os(OSX)
 import AppKit
 
+/// Abstration for NSView
 public typealias View = NSView
+
+/// Abstration for NSLayoutGuide
 public typealias LayoutGuide = NSLayoutGuide
+
+/// Abstration for NSLayoutConstraint.Priority
 public typealias LayoutPriority = NSLayoutConstraint.Priority
+
+/// Abstration for NSEdgeInsets
 public typealias EdgeInsets = NSEdgeInsets
 
 #else
 import UIKit
 
+/// Abstration for UIView
 public typealias View = UIView
+
+/// Abstration for UILayoutGuide
 public typealias LayoutGuide = UILayoutGuide
+
+/// Abstration for UILayoutPriority
 public typealias LayoutPriority = UILayoutPriority
+
+/// Abstration for UIEdgeInsets
 public typealias EdgeInsets = UIEdgeInsets
 
 #endif
