a6985fc33f2587eea4de7339b570e16302d28a21
Diogo Autilio <diautilio@gmail.com>
2022-11-06 00:42:50 -0300

    Update iOS simulator version

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
-            -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=16.0,name=iPhone\ 11 \
             clean test
