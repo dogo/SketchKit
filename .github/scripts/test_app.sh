61fbef73eccf9415b25dd6e4c6dd8cf7c3257b80
Diogo Autilio <diautilio@gmail.com>
2022-11-06 01:24:47 -0300

    enableCodeCoverage

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,4 +5,5 @@ set -eo pipefail
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=16.0,name=iPhone\ 11 \
+            -enableCodeCoverage YES \
             clean test
