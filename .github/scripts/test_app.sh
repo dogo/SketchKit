be6889aad8d8f877a0f700443b729c0c85076340
Diogo Autilio <diautilio@gmail.com>
2022-11-06 01:57:26 -0300

    Dont clean builds

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -6,4 +6,4 @@ xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
             -destination platform=iOS\ Simulator,OS=16.0,name=iPhone\ 11 \
             -enableCodeCoverage YES \
-            clean test
+            build test
