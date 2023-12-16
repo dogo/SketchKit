108c16cfc15da3e8a44dba0659205c79039e456f
Diogo Autilio <diautilio@gmail.com>
2023-12-16 08:20:08 -0300

    Upgrade MacOS

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 xcodebuild -project SketchKit.xcodeproj \
 			-scheme SketchKit-Package \
-            -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=17.0.1,name=iPhone\ 11 \
             -enableCodeCoverage YES \
             build test
